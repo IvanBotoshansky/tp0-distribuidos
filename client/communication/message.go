092f5dfd193cd3ad5842ff40c3e29d4af9724c5f
IvanBotoshansky <ibotoshansky@fi.uba.ar>
2025-03-19 16:19:42 -0300

    Added protocolo de comunicacion client

---
+++ client/communication/message.go
@@ -0,0 +1,89 @@
+package communication
+
+import (
+    "net"
+    "bytes"
+    "encoding/binary"
+)
+
+const LengthHeaderSize = 4
+
+// BetMessage represents the message sent by the client
+type BetMessage struct {
+    FirstName string
+    LastName  string
+    Document  string
+    Birthdate string
+    Number    string
+}
+
+// ConfirmationMessage represents the message received by the client
+type ConfirmationMessage struct {
+    Status string
+}
+
+// NewBetMessage creates a new BetMessage instance
+func NewBetMessage(firstName, lastName, document, birthdate, number string) BetMessage {
+    return BetMessage{
+        FirstName: firstName,
+        LastName:  lastName,
+        Document:  document,
+        Birthdate: birthdate,
+        Number:    number,
+    }
+}
+
+// NewConfirmationMessage creates a new ConfirmationMessage instance
+func NewConfirmationMessage(status string) ConfirmationMessage {
+    return ConfirmationMessage{
+        Status: status,
+    }
+}
+
+// SendMessage sends a message through the connection
+func SendMessage(conn net.Conn, data []byte) error {
+    totalBytesSent := 0
+    for totalBytesSent < len(data) {
+        bytesSent, err := conn.Write(data[totalBytesSent:])
+        if err != nil {
+            return err
+        }
+        totalBytesSent += bytesSent
+    }
+    return nil
+}
+
+// ReceiveMessage receives a message through the connection
+func ReceiveMessage(conn net.Conn, length uint32) ([]byte, error) {
+    totalBytesReceived := 0
+    data := make([]byte, length)
+    for totalBytesReceived < int(length) {
+        bytesReceived, err := conn.Read(data[totalBytesReceived:])
+        if err != nil {
+            return nil, err
+        }
+        totalBytesReceived += bytesReceived
+    }
+    return data, nil
+}
+
+// ReadMessagePayload reads the payload of a message from the connection
+func ReadMessagePayload(conn net.Conn) ([]byte, error) {
+    lengthBytes, err := ReceiveMessage(conn, LengthHeaderSize)
+    if err != nil {
+        return nil, err
+    }
+
+    var length uint32
+    buf := bytes.NewReader(lengthBytes)
+    if err := binary.Read(buf, binary.BigEndian, &length); err != nil {
+        return nil, err
+    }
+
+    payloadBytes, err := ReceiveMessage(conn, length)
+    if err != nil {
+        return nil, err
+    }
+    
+    return payloadBytes, nil
+}
---
+++ client/communication/serialization.go
@@ -0,0 +1,35 @@
+package communication
+
+import (
+    "bytes"
+    "encoding/binary"
+    "fmt"
+)
+
+// SerializeData serializes data
+func SerializeData(data string) ([]byte, error) {
+    dataBytes := []byte(data)
+    length := uint32(len(dataBytes))
+    buf := new(bytes.Buffer)
+    
+    if err := binary.Write(buf, binary.BigEndian, length); err != nil {
+        return nil, err
+    }
+    
+    if _, err := buf.Write(dataBytes); err != nil {
+        return nil, err
+    }
+    
+    return buf.Bytes(), nil
+}
+
+// SerializeBet serializes a bet message
+func SerializeBet(bet BetMessage) ([]byte, error) {
+    data := fmt.Sprintf("%s,%s,%s,%s,%s", bet.FirstName, bet.LastName, bet.Document, bet.Birthdate, bet.Number)
+    return SerializeData(data)
+}
+
+// DeserializeConfirmation deserializes data bytes of a confirmation message
+func DeserializeConfirmation(dataBytes []byte) ConfirmationMessage {
+	return NewConfirmationMessage(string(dataBytes))
+}
